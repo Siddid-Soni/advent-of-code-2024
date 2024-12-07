e0b8caaa11b3f0779b13e087678d99069c2cd0af
Siddid-Soni <siddidsoni@gmail.com>
2024-12-07 18:17:22 +0530

    day 6 speed up

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -1,56 +1,86 @@
 use crate::parse_input;
 use std::path::Path;
-use std::collections::HashSet;
 
+#[allow(non_snake_case)]
 pub fn process<P>(filename: P) -> usize where P: AsRef<Path>  {
+    let dirs: [(i32, i32); 4] = [(-1,0),(0,1),(1,0),(0,-1)];
     let mut grid = parse_input(filename);
-    let rows = grid.len();
-    let cols = grid[0].len();
+    let H = grid.len();
+    let W = grid[0].len();
     
-    let mut r = 0;
-    let mut c = 0;
-    'outer: for i in 0..rows {
-        for j in 0..cols {
-            if grid[i][j] == 94 {
-                (r,c) = (i,j);
-                break 'outer;
+    let mut start = (0,0);
+    for row in 0..H {
+        for col in 0..W {
+            if grid[row][col] == 94 {
+                start = (row, col);
             }
         }
     }
     
-    let loops = |grid: Vec<Vec<u8>>, mut r, mut c| {
-        let mut seen: HashSet<(usize, usize, i32, i32)> = HashSet::new();
-        let mut dr: i32 = -1;
-        let mut dc: i32 = 0;
-    
+    let solve = |grid: &Vec<Vec<u8>>| {
+        let mut dir = 0;
+        let mut me = start;
+
+        let mut vis = vec![false;H*W*4];
+        // let mut turns = 0;
         loop {
-            seen.insert((r, c, dr, dc));
-            if r as i32 + dr < 0 || r as i32 + dr >= rows as i32 || c as i32 + dc < 0 || c as i32 + dc >= cols as i32 { return false }
+            // turns+=1;
+            let hash = (me.0 * W + me.1) * 4 + dir;
+            if vis[hash] {return true}
+            vis[hash] = true;
+            // if turns == (H*W) {
+            //     return true;
+            // }
+
+            let r2 = me.0 as i32 + dirs[dir].0;
+            let c2 = me.1 as i32 + dirs[dir].1;
             
-            if grid[(r as i32 + dr) as usize][(c as i32 + dc) as usize] == 35 {
-                std::mem::swap(&mut dr, &mut dc);
-                dc *= -1;
-            } else {
-                r = (r as i32 + dr) as usize;
-                c = (c as i32 + dc) as usize;
+            if !(0<=r2 && r2<H as i32 && 0<=c2 && c2<W as i32) {
+                return false;
             }
-            if seen.contains(&(r,c,dr,dc)) { return true }
+            
+            if grid[r2 as usize][c2 as usize] == 35 {
+                dir = (dir+1)%4;
+            } else {
+                me = (r2 as usize,c2 as usize);
+            }
         }
     };
 
-    let mut count = 0;
+    let write_path = |grid: &mut Vec<Vec<u8>>| {
+        let mut dir = 0;
+        let mut me = start;
 
-    for i in 0..rows {
-        for j in 0..cols {
-            if grid[i][j]!=46 {continue}
-            grid[i][j] = 35;
-            if loops(grid.clone(), r.clone(), c.clone()) {
+        loop {
+            grid[me.0][me.1] = 88;
+
+            let r2 = me.0 as i32 + dirs[dir].0;
+            let c2 = me.1 as i32 + dirs[dir].1;
+            
+            if !(0<=r2 && r2<H as i32 && 0<=c2 && c2<W as i32) {
+                break;
+            }
+            
+            if grid[r2 as usize][c2 as usize] == 35 {
+                dir = (dir+1)%4;
+            } else {
+                me = (r2 as usize,c2 as usize);
+            }
+        }
+    };
+
+    write_path(&mut grid);
+    let mut count = 0;
+    for row in 0..H {
+        for col in 0..W {
+            if grid[row][col]!=88 {continue}
+            grid[row][col] = 35;
+            if solve(&grid) {
                 count+=1;
             }
-            grid[i][j] = 46;
+            grid[row][col] = 46;
         }
     }
-
     count
 }
 
